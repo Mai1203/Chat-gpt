fab75b9da53b4b6185b51e024381fa4ff96d17c0
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-07-06 12:49:39 -0500

    update Worker

--- worker.js
+++ worker.js
@@ -1,8 +1,15 @@
-onmessage = (e) => {
-  console.log("Worker: Mensaje recibido");
-  console.log(e);
-  if(e.data.name === 'hello'){
-    console.log("enviando mensaje");
-    postMessage({ name: 'hello from back'})
-  }
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+
+const handler = new WebWorkerMLCEngineHandler()
+
+self.onmessage = (msg) => {
+  handler.onmessage(msg)
 }
+// onmessage = (e) => {
+//   console.log("Worker: Mensaje recibido");
+//   console.log(e);
+//   if(e.data.name === 'hello'){
+//     console.log("enviando mensaje");
+//     postMessage({ name: 'hello from back'})
+//   }
+// }
